c3eb5ae14e9db791c32b468ee79384bac8ce1d4f
Chayapol Limanon <chayapol.limanon@gmail.com>
2019-06-11 11:13:06 +0700

    Allow some fields to be optional.

--- src/LogstashOption.ts
+++ src/LogstashOption.ts
@@ -1,10 +1,10 @@
 import { LoggerOptions } from "winston";
 
 export interface LogstashOption extends LoggerOptions {
-  application: string;
-  hostname: string;
+  application?: string;
+  hostname?: string;
   host: string;
   port: number;
-  trailingLineFeed: boolean;
-  trailingLineFeedChar: string;
+  trailingLineFeed?: boolean;
+  trailingLineFeedChar?: string;
 }
