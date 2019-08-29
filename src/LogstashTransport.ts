115ab40f29f319f769a257340a7f81293daebb0c
Chayapol Limanon <chayapol.limanon@gmail.com>
2019-08-29 14:19:58 +0700

    v0.2.0

--- src/LogstashTransport.ts
+++ src/LogstashTransport.ts
@@ -3,9 +3,10 @@ import * as net from "net";
 import * as os from "os";
 import * as winston from "winston";
 import * as Transport from "winston-transport";
-import * as logform from "logform"
 import { LogstashOption } from "./LogstashOption";
-import { disconnect } from "cluster";
+import * as debugging from "debug";
+
+const debug = debugging("winston-logstash-ts:LogStashTransport");
 
 export class LogstashTransport extends Transport {
 
@@ -13,33 +14,48 @@ export class LogstashTransport extends Transport {
   protected host: string;
   protected port: number;
   protected protocol: "tcp" | "udp";
-  protected udpClient: dgram.Socket;
-  protected tcpClient: net.Socket;
+  protected debug: debug.Debugger;
 
   protected tcpKeepAliveInitialDelay: number = 0;
 
   constructor(options?: LogstashOption) {
     super(options);
-
     this.host = options.host;
     this.port = options.port;
     this.silent = options.silent;
     this.protocol = options.protocol || "udp"
-    this.tcpKeepAliveInitialDelay = options.tcpKeepAliveInitialDelay || 0; 
-
-    this.udpClient = null;
-    this.tcpClient = null;
+    this.tcpKeepAliveInitialDelay = options.tcpKeepAliveInitialDelay || 0;
   }
 
-  public connect() {
+  public async connect() {
     if (this.protocol === "udp") {
-      this.udpClient = dgram.createSocket("udp4");
-      this.udpClient.unref();
+      const udpClient = dgram.createSocket("udp4");
+      udpClient.unref();
+      return udpClient;
     } else if (this.protocol === "tcp") {
-      this.tcpClient = new net.Socket();
-      this.tcpClient.connect(this.port, this.host);
-      this.tcpClient.setKeepAlive(true, this.tcpKeepAliveInitialDelay)
-      this.tcpClient.unref();
+      return new Promise<net.Socket>((resolve, reject) => {
+        const tcpClient = new net.Socket();
+
+        const errorListener = (error) => {
+          debug.color = "196";
+          debug("%o", error);
+          tcpClient.destroy();
+          reject(error);
+        }
+
+        const connectListener = () => {
+          debug.color = "46";
+          debug("TCP connection to %s:%d has been established.", this.host, this.port);
+          tcpClient.setKeepAlive(true, this.tcpKeepAliveInitialDelay);
+          tcpClient.unref();
+          tcpClient.removeListener("connect", connectListener);
+          resolve(tcpClient);
+        };
+
+        tcpClient.on("error", errorListener)
+        tcpClient.on("connect", connectListener);
+        tcpClient.connect(this.port, this.host);
+      })
     } else {
       throw new Error("Invalid protocol, only support TCP and UDP.")
     }
@@ -53,46 +69,45 @@ export class LogstashTransport extends Transport {
       .then((result) => {
         this.emit("logged", result);
       })
-      .catch((err) => {
-        callback(err, !err);
+      .catch((error) => {
+        debug.color = "196"
+        debug("An unexpected error occured, transporting to logstash is disabled now.", error.stack);
       })
   }
 
   public async send(message, callback) {
-    return new Promise((resolve, reject) => {
-      const transformed = JSON.stringify(this.format.transform(JSON.parse(message)));
-      const buf = Buffer.from(transformed);
+    const transformed = JSON.stringify(this.format.transform(JSON.parse(message)));
+    const buf = Buffer.from(transformed);
 
-      if (this.protocol === "udp") {
-        if (!this.udpClient) {
-          this.connect();
+    if (this.protocol === "udp") {
+      const udpClient: dgram.Socket = (await this.connect()) as dgram.Socket;
+      udpClient.send(buf, 0, buf.length, this.port, this.host, (error, bytes) => {
+        if (callback) {
+          callback(error, bytes);
         }
-        this.udpClient.send(buf, 0, buf.length, this.port, this.host, (error, bytes) => {
-          if (error) {
-            reject(error)
-          } else {
-            resolve(bytes);
-          }
-          if (callback) {
-            callback(error, bytes);
-          }
+      });
+    } else {
+      try {
+        const tcpClient: net.Socket = (await this.connect()) as net.Socket;
+        await new Promise((resolve, reject) => {
+          tcpClient.write(transformed, (error) => {
+            if (callback) {
+              callback(error);
+            }
+            if (error) {
+              reject(error)
+            } else {
+              resolve();
+            }
+          });
         });
-      } else {
-        this.connect();
-        this.tcpClient.write(transformed, (error) => {
-          if (error) {
-            this.tcpClient,disconnect();
-            reject(error)
-          } else {
-            this.tcpClient,disconnect();
-            resolve();
-          }
-          if (callback) {
-            callback(error);
-          }
-        });
+      } catch (error) {
+        if (callback) {
+          callback(error);
+        }
+        throw error;
       }
-    })
+    }
   }
 
   public static createLogger(logType: string, logstashOption: LogstashOption) {
